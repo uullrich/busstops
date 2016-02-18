bef5e2ed939e883dcdfd74deb6365117766ce417
Uwe Ullrich <ullrich@uwes-air.fritz.box>
2016-02-18 22:54:18 +0100

    Added frontend logic for a new busstop

--- public/js/app/newBusstop.js
+++ public/js/app/newBusstop.js
@@ -1,45 +1,62 @@
 define(['jquery', 'newBusstopMap', 'bootstrap', 'timepicker', 'moment'], function($, map, bootstrap, timepicker, moment){
-	var NewBusstopUILogic = {
+	
+    var NewBusstopUILogic = {
     	
     	busnumbers: [],
         
         selectedBusnumber: {},
 
     	addBusnumber: function(){
-			$("#newBusnumberButton").click(function(e){
-				e.preventDefault();
-				$(".newBusnumberWraper").show();
-				$("#newBusnumberButton").hide();
-			});
 			
-			$("#addBusnumberButton").click(function(e){
+            //Logic for creating a new busnumber
+            var addBusnumberLogic = function(){
+                
+                var newBusnumber = $("#busnumberInput").val();
+
+                if(newBusnumber === ""){
+                    alert("Please insert a busnumber!");
+                }else{
+                    
+                    $(".newBusnumberWraper").hide();
+                    $("#newBusnumberButton").show();
+                    
+                    if(!NewBusstopUILogic.containsBusnumber(newBusnumber)){
+                        $(".busnumberList").append('<li><a href="#">' + newBusnumber + '</a></li>');
+                        NewBusstopUILogic.selectBuslineInit();
+
+                        NewBusstopUILogic.busnumbers.push({
+                            line: newBusnumber,
+                            departure: []
+                        });
+
+                        //Empty the input field
+                        $("#busnumberInput").val("");
+                    }
+                }
+            };
+
+            //Eventhandlers
+            $("#newBusnumberButton").off("click").on("click", function(e){
+                e.preventDefault();
+                $(".newBusnumberWraper").show();
+                $("#newBusnumberButton").hide();
+            });
+
+			$("#addBusnumberButton").off("click").on("click", function(e){
 				e.preventDefault();
-
-				var newBusnumber = $("#busnumberInput").val();
-
-				if(newBusnumber === ""){
-					alert("Please insert a busnumber!");
-
-				}else{
-					
-					$(".newBusnumberWraper").hide();
-					$("#newBusnumberButton").show();
-					
-					if(!NewBusstopUILogic.containsBusnumber(newBusnumber)){
-						$(".busnumberList").append('<li><a href="#">' + newBusnumber + '</a></li>');
-
-						NewBusstopUILogic.busnumbers.push({
-							line: newBusnumber,
-							departure: []
-						});
-
-						//Empty the input field
-						$("#busnumberInput").val("");
-					}
-				}
+                addBusnumberLogic();
 			});
 
-			$("#cancelBusnumberButton").click(function(e){
+            $("#busnumberInput").keypress(function (ev) {
+                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+                if (keycode === 13) {
+                    ev.preventDefault();
+                    addBusnumberLogic();
+                    return false;
+                }                
+            });
+
+			$("#cancelBusnumberButton").off("click").on("click", function(e){
 				e.preventDefault();
 				$(".newBusnumberWraper").hide();
 				$("#newBusnumberButton").show();
@@ -59,9 +76,26 @@ define(['jquery', 'newBusstopMap', 'bootstrap', 'timepicker', 'moment'], functio
     		return false;
     	},
         
+        selectBuslineInit: function(){
+            $('.dropdown-menu li a').off( "click").on("click", function(e){
+                var selected = $(this).text();
+                
+                NewBusstopUILogic.busnumbers.map(function(busnumber){
+                    if(busnumber.line === selected){
+                        NewBusstopUILogic.selectedBusnumber = busnumber;
+                    }
+                });
+
+                $("#selectedLineLabel").html(NewBusstopUILogic.selectedBusnumber.line);
+                //console.log("Selected" + NewBusstopUILogic.selectedBusnumber.line);
+            });
+        },
+
         addDepartureTime: function(){
-            $("#addTime").click(function(e) {
-                e.preventDefault();                
+            
+            //Logik for a new departure Time
+            var addDepartureTimeLogic = function() {
+                               
                 //alert("Timepicker value: " +  + " moment " + moment());
               
                 var departureTimeInDateFormat = moment($(".timepicker").val(), ['h:m a', 'H:m']).toDate();
@@ -74,22 +108,38 @@ define(['jquery', 'newBusstopMap', 'bootstrap', 'timepicker', 'moment'], functio
                 });
                 
                 NewBusstopUILogic.appendNewDeparterTime();
+            }
+
+            //Eventhandlers
+            $(".timepicker").keypress(function (ev) {                
+                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+                if (keycode === 13) {
+                    ev.preventDefault();
+                    addDepartureTimeLogic();
+                    return false;
+                }                
+            });
+
+            $("#addTime").off("click").on("click", function(ev){
+                ev.preventDefault(); 
+                addDepartureTimeLogic();
                 return false;
-            })  
+            });  
         },
         
         appendNewDeparterTime: function(){
               
         },
-        
+
         initTimepickers: function(){
             $(".timepicker").timepicker();
         },
 
     	init: function(){
+            NewBusstopUILogic.selectBuslineInit();
 			NewBusstopUILogic.addBusnumber();
             NewBusstopUILogic.addDepartureTime();
-            NewBusstopUILogic.initTimepickers();
+            NewBusstopUILogic.initTimepickers(); 
     	}
     }
 
